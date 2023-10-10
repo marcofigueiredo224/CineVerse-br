d9296e9fdebbe1a3ad7119e3265b2a10f7afff27
Marco Figueiredo <marco.figueiredo@blueservice.com.br>
2023-10-09 23:28:53 -0300

    first commit

---
+++ camps_pesquisa.js
@@ -0,0 +1,10 @@
+
+function clickPesquisar() {
+    if (campo_pesquisa.style.display == "block") {
+        campo_pesquisa.style.display = "none";
+    } else{
+        campo_pesquisa.style.display = "block";
+    }
+}
+
+
---
+++ carousel.js
@@ -0,0 +1,79 @@
+const minWidthForScript = 200;
+const windowWidth = window.innerWidth;
+
+function setupCarousel(carouselId, prevBtnId, nextBtnId) {
+    if (windowWidth >= minWidthForScript) {
+        const leftArrow = document.getElementById(prevBtnId);
+        const rightArrow = document.getElementById(nextBtnId);
+        const carousel = document.getElementById(carouselId);
+        const projetos = document.querySelectorAll('.img-series');
+
+        let currentSlide = 0;
+        const totalSlides = projetos.length;
+
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % totalSlides;
+            scrollToSlide(currentSlide);
+        }
+
+        function prevSlide() {
+            currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+            scrollToSlide(currentSlide);
+        }
+
+        function scrollToSlide(slideIndex) {
+            const slideWidth = projetos[0].getBoundingClientRect().width;
+            carousel.scrollLeft = slideWidth * slideIndex;
+        }
+
+        leftArrow.addEventListener('click', prevSlide);
+        rightArrow.addEventListener('click', nextSlide);
+    }
+}
+
+setupCarousel('carousel', 'prevBtn', 'nextBtn');
+setupCarousel('carousel1', 'prevBtn1', 'nextBtn1');
+setupCarousel('carousel2', 'prevBtn2', 'nextBtn2');
+setupCarousel('carousel3', 'prevBtn3', 'nextBtn3');
+
+
+
+
+
+
+function setupSmoothScroll(carouselId) {
+    let isDragging = false;
+    let startX;
+    let scrollLeft;
+
+    const carousel = document.getElementById(carouselId);
+
+    carousel.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        startX = e.pageX - carousel.offsetLeft;
+        scrollLeft = carousel.scrollLeft;
+    });
+
+    carousel.addEventListener('mouseleave', () => {
+        isDragging = false;
+    });
+
+    carousel.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+
+    carousel.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+        e.preventDefault();
+        const x = e.pageX - carousel.offsetLeft;
+        const walk = (x - startX) * 1.5; 
+
+        carousel.scrollLeft = scrollLeft - walk;
+    });
+}
+
+
+setupSmoothScroll('carousel');
+setupSmoothScroll('carousel1');
+setupSmoothScroll('carousel2');
+setupSmoothScroll('carousel3');
