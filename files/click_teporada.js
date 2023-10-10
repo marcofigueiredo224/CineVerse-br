74a3fa9ba27ec7661b49bd13ea504cde2f8bcf75
Marco Figueiredo <marco.figueiredo@blueservice.com.br>
2023-10-10 20:53:55 -0300

    teste

---
+++ files/click_teporada.js
@@ -0,0 +1,25 @@
+function clickTemporada(id) {
+    var temporada_eps = document.getElementById(id);
+
+    if (temporada_eps.style.display == "block") {
+        temporada_eps.style.display = "none";
+    } else {
+        temporada_eps.style.display = "block";
+    }
+}
+function clickTemporada2(id) {
+    var temporada_eps2 = document.getElementById(id);
+
+    if (temporada_eps2.style.display == "block") {
+        temporada_eps2.style.display = "none";
+    } else {
+        temporada_eps2.style.display = "block";
+    }
+}
+
+
+
+
+
+
+
